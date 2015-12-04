f4a5ee4cd9c498ae4bc8bb8151d600d6ddfed4eb
Artur Salamon <artur.salamon@gmail.com>
2015-12-04 21:28:10 +0100

    moje myslenie aby to zrobic ladniej

--- src/PPJz_09/Wyraz2.java
+++ src/PPJz_09/Wyraz2.java
@@ -14,7 +14,18 @@ public class Wyraz2 {
 
     public void setNastepnyWyraz(String x){
         this.b = new Wyraz2(x);
+
+        while(this.b != null)
+     //       this = this.b;
+        this.b = new Wyraz2(x);
+
+
+
     }
+
+
+
+
 
     public void show(){
         System.out.print(a);
